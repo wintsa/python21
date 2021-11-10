49e43ac6ccd82eac0d6c4103ceaf843a17c19c44
wintsa <wintsa@gmail.com>
2021-11-10 11:55:31 +0300

    PyGame 4

--- pygame_begin.py
+++ pygame_begin.py
@@ -6,12 +6,6 @@ pygame.init()
 WIDTH = 1200
 HEIGHT = 800
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-ship = pygame.image.load('img/ship.png')
-ship = pygame.transform.rotate(ship, 90)
-
-"""Координаты корабля"""
-shipX = WIDTH // 2
-shipY = HEIGHT // 2
 
 font = pygame.font.SysFont('comicsansms', 40)
 clock = pygame.time.Clock()
@@ -20,6 +14,28 @@ mouse_position = (0, 0)
 
 """Выстрелы на экране"""
 fires = []
+
+
+class Ship:
+    """
+    Класс для отображения корабля
+    """
+
+    def __init__(self, x, y, imgPath):
+        """
+        Конструктор корабля
+        x, y - горизонтальная и вертикальная координата
+        """
+        self.x = x
+        self.y = y
+        self.img = pygame.image.load(imgPath)
+        self.img = pygame.transform.rotate(self.img, 90)
+
+    def drawOn(self, screen):
+        rect = pygame.Rect((self.x - 100, self.y), (200, 70))
+        pygame.draw.rect(screen, THECOLORS['green'], rect, 0)
+        screen.blit(self.img, (self.x - 60, self.y - 50))
+
 
 class Fire:
     """
@@ -55,19 +71,21 @@ class Fire:
         """
         return self.x < 0 or self.y < 0 or self.x > WIDTH or self.y > HEIGHT
 
+    def drawOn(self, screen):
+        pygame.draw.circle(screen, THECOLORS['orange'], (self.x, self.y), 10)
+
+
+ship = Ship(WIDTH // 2, HEIGHT // 2, 'img/ship.png')
+
 
 def draw():
     """
     Основная функция отрисовки
     """
-    global numFrame
     screen.fill(THECOLORS['white'])
-    rect = pygame.Rect((shipX - 100, shipY), (200, 70))
-    pygame.draw.rect(screen, THECOLORS['green'], rect, 0)
-    screen.blit(ship, (shipX - 60, shipY - 50))
+    ship.drawOn(screen)
 
     text = font.render(str(numFrame), True, THECOLORS['black'])
-    numFrame += 1
     screen.blit(text, (0, HEIGHT - text.get_height()))
 
     text = font.render(str(mouse_position), True, THECOLORS['black'])
@@ -78,7 +96,7 @@ def draw():
 
     for fire in fires:
         fire.move()
-        pygame.draw.circle(screen, THECOLORS['orange'], (fire.x, fire.y), 10)
+        fire.drawOn(screen)
         if fire.out():
             fires.remove(fire)
 
@@ -94,25 +112,27 @@ while True:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                shipY -= 10
+                ship.y -= 10
             elif event.key == pygame.K_DOWN:
-                shipY += 10
+                ship.y += 10
             elif event.key == pygame.K_LEFT:
-                shipX -= 10
+                ship.x -= 10
+                if ship.x < 0:
+                    ship.x = 0
             elif event.key == pygame.K_RIGHT:
-                shipX += 10
+                ship.x += 10
 
         if event.type == pygame.MOUSEMOTION:
             mouse_position = pygame.mouse.get_pos()
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
-            dx = pos[0] - shipX
-            dy = pos[1] - shipY
+            dx = pos[0] - ship.x
+            dy = pos[1] - ship.y
             lenXY = sqrt(dx * dx + dy * dy)
             dx = dx * 3 // lenXY
             dy = dy * 3 // lenXY
-            print(dx, dy)
-            fires.append(Fire(shipX, shipY, dx, dy))
+            fires.append(Fire(ship.x, ship.y, dx, dy))
 
+    numFrame += 1
     draw()
