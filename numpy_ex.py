732b40d4fce6b7b3bcc13928b0270a66b0286496
wintsa <wintsa@gmail.com>
2021-11-22 13:04:03 +0300

    a lot of things

--- numpy_ex.py
+++ numpy_ex.py
@@ -1,15 +1,22 @@
 import numpy as np
 
-map = np.array([[2, 3, 5, 3],
-                [1, 2, 3, 4],
-                [3, 7, 1, 1]])
 
-a = np.ones((6,2), dtype=int)
+
+python2d = [[2, 3, 5, 3],
+            [1, 2, 3, 4],
+            [3, 7, 1, 1]]
+map = np.array(python2d)
+
+print(python2d[1][1], map[1, 1])
+
+print(map.dtype)
+
+a = np.ones((6, 2), dtype=int)
 b = np.zeros_like(map)
 d = np.ones_like(map)
-c = map*2 + 1
+c = map * 2 + 1
 
-e = a.reshape((3,4)) + c
+e = a.reshape((3, 4)) + c
 
 print(map, a, b, c, e, sep='\n')
 
@@ -17,3 +24,15 @@ print(np.cumprod(map).reshape(map.shape))
 
 print(map.shape)
 
+np.info(np.ndarray.dtype)
+
+comp = a.reshape(map.shape) == map
+
+h = np.random.random((3, 3, 3))
+
+print(h)
+
+t = [[0, 1], [0, 0], [0, 0]]
+
+print(h[t])
+print(h[h > 0.5])
--- simple.py
+++ simple.py
@@ -1,14 +1,50 @@
 import math
 
-def add(a, b):
+
+def add(a: int, b: int) -> int:
     """
     Функция складывает два числа
     """
-    return a+b
+    return a + b
 
-if __name__=='__main__':
+class T:
+    x = 0
+    def __init__(self, x):
+        self.x = x
+
+    def __add__(self, other):
+        if isinstance(other, T):
+            return T(self.x + other.x)
+        if isinstance(other, int):
+            return T(self.x + other)
+        return None
+
+    def __str__(self):
+        return str(self.x)
+
+    def __repr__(self):
+        return 'r'+str(self.x)
+
+
+if __name__ == '__main__':
+    """
+    # s = {1, 2, 3, 1, 2, 3}
+    l = [1, 2, 3, 1, 2, 3]
+    t = (1, 2, 3, 1, 2, 3)
+
+    s = set(l)
+    l.append(2)
+    print(s, l, t)
     a = False or True and False or True
     print(a)
+    """
     '''l = [input(), input()]
     a, b = map(int, l)
     print(add(a,b))'''
+
+    a = T(3)
+    b = T(4)
+    c = a + b
+    d = a + 5
+    print(c, d)
+    print([c, d])
